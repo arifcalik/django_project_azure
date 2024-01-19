5a260c52f7026e91886db0311388f8bdc20e105e
arifcalik <arifcalik@gmail.com>
2024-01-19 16:12:07 +0100

    Some fixes in settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('django_project/static')),)
+
+STATIC_URL = '/django_project/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
